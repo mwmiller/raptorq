720cc361a0b813236efd5fb95aee5a8a46ad2bdf
Matthew Miller <mwm@fastmail.com>
2025-05-13 13:13:30 +0200

    Add Deg generator
    I remember now that this was what annoyed me last time I looked at the
    previous iteration of Raptor.  `Deg[v]` generator uses the `k_prime`
    value.  How is it not in the parameters list?
    
    We can see from the tests that the effect is pretty minimal, but it
    still exists.. so include it in the required parameters!

--- lib/raptorq/generators.ex
+++ lib/raptorq/generators.ex
@@ -6,6 +6,57 @@ defmodule Raptorq.Generators do
 
   import Bitwise
   import Raptorq.Lookup
+  alias Raptorq.SIOP
+
+  @key_re ~r/(?<i0>\d+)[^\d]+(?<f0>\d+)[^\d]+((?<i1>\d+)[^\d]+(?<f1>\d+))?\s/
+  @degrees :code.priv_dir(:raptorq)
+           |> Path.join("Deg.table")
+           |> File.read!()
+           |> String.split("\n", trim: true)
+           |> Enum.reduce([], fn line, acc ->
+             case Regex.named_captures(@key_re, line) do
+               # Last line special case
+               %{"i0" => i0, "f0" => f0, "i1" => "", "f1" => ""} ->
+                 [{String.to_integer(f0), String.to_integer(i0)} | acc]
+
+               %{"i0" => i0, "f0" => f0, "i1" => i1, "f1" => f1} ->
+                 [
+                   {String.to_integer(f0), String.to_integer(i0)},
+                   {String.to_integer(f1), String.to_integer(i1)}
+                   | acc
+                 ]
+
+               _ ->
+                 acc
+             end
+           end)
+           |> Enum.sort(:desc)
+
+  @doc """
+  Generate the degrees for a given value and k_prime.
+
+  Note that the RFC formulation suggests that it depends only on `v` but
+  then uses `k_prime` to limit the maximum value.
+
+  This uses `:exact` value matching for `k_prime`.
+
+  ## Parameters
+  - `v`: Non-negative integer between 0 and 1,048,576 (2^20)
+  - `k_prime`: Non-negative integer in the SIOP table
+  """
+
+  def deg(v, k_prime)
+      when is_integer(v) and v >= 0 and v <= 1_048_576 and is_integer(k_prime) do
+    {_, dtable} = Enum.find(@degrees, {0, 0}, fn {f, _} -> v >= f end)
+    # This will raise on improper k_prime so we can just match
+    %{w: w} = SIOP.values_for(k_prime, :exact)
+    min(dtable, w - 2)
+  end
+
+  def deg(v, k_prime) do
+    raise ArgumentError,
+          "Invalid arguments for deg function. Integer for v on 0..2^20 (#{v}) and k_prime (#{k_prime})."
+  end
 
   @doc """
   Generates a number between 0 and m-1
--- test/raptorq_generators_test.exs
+++ test/raptorq_generators_test.exs
@@ -19,4 +19,21 @@ defmodule RaptorqGeneratorsTest do
     assert rand(2048, 0, 1024 ** 10) == 4_190_514_305
     assert rand(2048, 255, 1024 ** 10) == 1_513_049_617
   end
+
+  test "deg" do
+    assert_raise ArgumentError, fn -> deg(-1, 1) end
+    assert_raise ArgumentError, fn -> deg(1024, 9) end
+    assert deg(0, 10) == 0
+    assert deg(5242, 10) == 0
+    assert deg(5243, 10) == 1
+    assert deg(529_531, 10) == 2
+    assert deg(1_048_576, 10) == 15
+    assert deg(1_048_576, 18) == 27
+    assert deg(1_048_576, 18) == 27
+    assert deg(1_048_576, 20) == 29
+    assert deg(1_048_576, 26) == 30
+    assert deg(1_017_662, 26) == 29
+    assert deg(1_017_662, 1777) == 29
+    assert_raise ArgumentError, fn -> deg(1_017_662, 1_234_567) end
+  end
 end
